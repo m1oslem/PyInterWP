caaac0ecfaa749824353a07b1ce9b862ab5ac3d1
Eng Muslim Al-Shamary <m1oslem@gmail.com>
2025-04-26 19:19:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def simplify_error_message(error_msg):
         return error_msg
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
